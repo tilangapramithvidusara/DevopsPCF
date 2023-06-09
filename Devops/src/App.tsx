a3c0cd7de3e928c09ac43a4a9db5e13b46c11e66
user <tilanga.pramith@1billiontech.com>
2023-06-09 13:14:25 +0530

    initial commit

---
+++ Devops/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConnectionContainer from './Features/ConnectionContainer';
+
+export default function App() {
+  return (
+    <div>
+      <ConnectionContainer/>
+    </div>
+  )
+}
---
+++ Devops/src/Features/ConnectionContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ConnectionContainer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
